e8ca5d4d4e3340974882bb8a7502129b25242c21
moturuab <abhishek.moturu@mail.utoronto.ca>
2025-11-25 23:03:06 -0500

    try with clean

--- run_killarney.sh
+++ run_killarney.sh
@@ -4,9 +4,6 @@ groupid=$(date +%F_%T)
 python train_clothing1m.py \
   --train_npz /home/moturuab/projects/aip-agoldenb/moturuab/clothing1m.npz \
   --test_npz  /home/moturuab/projects/aip-agoldenb/moturuab/clothing10k_test.npz \
-  --model_name resnet50 \
-  --pretrained True \
-  --epochs 10 \
   --project clothing1m-lilaw-r50 $@
 
 #deactivate
